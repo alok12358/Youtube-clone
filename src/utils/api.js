dd5e52934d98c54921177ae10fa7a60e8702dc01
Alok patel <84653540+alok12358@users.noreply.github.com>
2024-05-05 16:02:05 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,8 +5,8 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
     params: { hl: "en", gl: "US" },
     headers: {
-        "X-RapidAPI-Key":'f79a04d4d6msh5db574ab01b9fc9p162b05jsn3868194e0e88',
-            // process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+        "X-RapidAPI-Key":
+             process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
